0f9f215a0bb54813762a6ea055d62e44051a850b
IverCoder <ivercoder@proton.me>
2024-04-08 11:43:47 +0800

    feat: audio sharing integration

--- config/scripts/systemd-homed-selinux.sh
+++ config/scripts/systemd-homed-selinux.sh
@@ -3,17 +3,23 @@
 set -oue pipefail
 
 rpm-ostree install policycoreutils-devel selinux-policy-devel setools-console
+echo "Necessary packages installed."
 git clone https://github.com/richiedaze/homed-selinux.git
+echo "SELinux rules repo cloned."
 cd homed-selinux
 make -f /usr/share/selinux/devel/Makefile homed.pp
+echo "SELinux rules built."
 sudo semodule --install=homed.pp
+echo "SELinux rules installed."
 sudo restorecon -rv \
     /usr/lib/systemd/systemd-homed \
     /usr/lib/systemd/systemd-homework \
     /usr/lib/systemd/system/systemd-homed.service \
     /usr/lib/systemd/system/systemd-homed-activate.service \
     /var/lib/systemd/home
+echo "SELinux rules applied."
 sudo authselect enable-feature \
     with-systemd-homed
+echo "Authselect support for systemd-homed enabled."
 cd ..
 rm -r homed-selinux
